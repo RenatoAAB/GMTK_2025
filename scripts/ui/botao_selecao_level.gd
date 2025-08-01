f2b0a553e60bf2e3ab752589904253a93a7fae9e
RenatoAAB <renatottin@hotmail.com>
2025-08-01 19:52:04 -0300

    resolve bug selecao de nivel

--- scripts/ui/botao_selecao_level.gd
+++ scripts/ui/botao_selecao_level.gd
@@ -8,10 +8,12 @@ extends Node
 @onready var animation_player: AnimationPlayer = $"../TitleScreen/AnimationPlayer"
 @onready var level_name_label: Label = $"../TitleScreen/LevelName"
 @onready var levelname_front: Label = $"../TitleScreen/LevelName/LevelnameFront"
-@onready var title_screen: CanvasLayer = $"../TitleScreen"
+@onready var title_screen: Control = $"../TitleScreen"
 
+var pressed = false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	animation_player.animation_finished.connect(_on_transition_finished)
 	number.text = level_number
 	if level_number in LevelManager.levels_cleared:
 		cleared.visible = true
@@ -22,13 +24,16 @@ func _process(delta: float) -> void:
 	
 
 func _on_botao_level_pressed() -> void:
+	print("Apertei o botao")
+	pressed = true
 	animation_player.play("transition_to_title")
 	title_screen.visible = true
 	level_name_label.text = level_name
 	levelname_front.text = level_name
 	
 
-func _on_animation_player_animation_finished(anim_name: StringName) -> void:
-	level_name_label.visible = true;
-	await get_tree().create_timer(1.0).timeout
-	LevelManager.load_level(level, level_number)
+func _on_transition_finished(anim_name):
+	if pressed:
+		level_name_label.visible = true;
+		await get_tree().create_timer(1.0).timeout
+		LevelManager.load_level(level, level_number)

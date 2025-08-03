82d1e2458944350744f3e1b7385fdd839d9314a7
RenanMartorelli <renanmartorelli2@gmail.com>
2025-08-03 01:15:24 -0300

    correcoes

--- scripts/ui/botao_selecao_level.gd
+++ scripts/ui/botao_selecao_level.gd
@@ -35,5 +35,5 @@ func _on_botao_level_pressed() -> void:
 func _on_transition_finished(anim_name):
 	if pressed:
 		level_name_label.visible = true;
-		await get_tree().create_timer(1.0).timeout
+		await get_tree().create_timer(2.0).timeout
 		LevelManager.load_level(level, level_number)
